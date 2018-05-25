1594046e0ecbb888f84f5e800fc34d6b0424cc54
mattsdot1 <mattsdot2@icloud.com>
2018-05-25 18:08:09 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -163,7 +163,7 @@ bot.on("guildMemberAdd", function (member){
 
 bot.on("ready", function (){
 	console.log("Mattify Reply is ready!");
-	bot.user.setActivity("with ?help ¯\_(ツ)_/¯");
+	bot.user.setActivity("with ?help ¯\_(ツ)_/¯ ");
 	bot.user.setStatus('dnd')
 	});
 

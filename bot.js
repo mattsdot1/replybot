fd2c22b7ca0cc355828295022f016576d7c34122
mattsdot1 <mattsdot2@icloud.com>
2018-05-26 20:06:38 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -207,8 +207,8 @@ bot.on("message", function (message)
             // delete from this line
             else
             {
-               message.channel.send( message.author + ", I know you love to say hello to me, but i can't do it forever! Please wait 5 seconds before saying hi again!");
-	    }
+               	message.channel.send( message.author + ", I know you love to say hello to me, but i can't do it forever! Please wait 5 seconds before saying hi again!");
+	    });
             // to this line if u don't want to send any message when the timer hasn't cooled down
 		}
 		if(msg.startsWith("sup"))
